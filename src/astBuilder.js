7d7b1db7aafb09bee62fd164039ef52fa2ddc5c1
Kirill Kostikov <kirill.kostikov.dev@gmail.com>
2021-01-12 23:03:32 +0300

    refactor astBuilder

---
+++ src/astBuilder.js
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+
+const buildNode = (data1, data2) => {
+  const keys = _.union(Object.keys(data1), Object.keys(data2));
+  const sortedKeys = _.sortBy(keys);
+
+  const ast = sortedKeys.map((key) => {
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return {
+        key,
+        type: 'added',
+        value: data2[key],
+      };
+    }
+
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return {
+        key,
+        type: 'removed',
+        value: data1[key],
+      };
+    }
+
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        key,
+        type: 'changed',
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
+
+    return {
+      key,
+      type: 'unchanged',
+      value: data2[key],
+    };
+  });
+
+  return ast;
+};
+
+const buildASTDiff = (data1, data2) => ({
+  type: 'root',
+  children: buildNode(data1, data2),
+});
+
+export default buildASTDiff;
