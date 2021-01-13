8accd2f0fd4cbd5f0656dee3827d4ec1af29fd98
Kirill Kostikov <kirill.kostikov.dev@gmail.com>
2021-01-13 16:47:11 +0300

    set default value to format

--- src/cli.js
+++ src/cli.js
@@ -5,17 +5,12 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-v, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format');
-
-program
-  .arguments('<filepath1> <filepath2>')
-  .action((filePath1, filePath2) => {
-    const diff = genDiff(filePath1, filePath2);
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2> [format]')
+  .action((filePath1, filePath2, format) => {
+    const diff = genDiff(filePath1, filePath2, format);
     console.log(diff);
   });
 
-export default () => {
-  program.parse(process.argv);
-};
+export default () => program.parse(process.argv);
