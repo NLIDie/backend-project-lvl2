8de51f737b01ec2ae4ce856806f9a9a1653b990b
Kirill Kostikov <kirill.kostikov.dev@gmail.com>
2021-01-13 18:41:33 +0300

    add tests for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,23 +11,37 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-let stylishResult;
+const outputResults = {};
 beforeAll(() => {
-  stylishResult = readFixture('result_stylish.txt');
+  outputResults.stylish = readFixture('result_stylish.txt');
+  outputResults.plain = readFixture('result_plain.txt');
 });
 
+const outputFormats = ['stylish', 'plain'];
+const filesExtensions = [
+  ['json', 'json'],
+  ['yml', 'yml'],
+  ['yaml', 'yaml'],
+
+  ['json', 'yml'],
+  ['json', 'yaml'],
+
+  ['yml', 'json'],
+  ['yaml', 'json'],
+];
+
 describe('genDiff', () => {
-  test('Should return correct diff for *.json files', () => {
-    const filePath1 = getFixturePath('file1.json');
-    const filePath2 = getFixturePath('file2.json');
+  outputFormats.forEach((outputFormat) => {
+    describe(`${outputFormat} format`, () => {
+      filesExtensions.forEach(([fileExt1, fileExt2]) => {
+        test(`Should return correct diff *.${fileExt1} *.${fileExt2} files`, () => {
+          const filePath1 = getFixturePath(`file1.${fileExt1}`);
+          const filePath2 = getFixturePath(`file2.${fileExt2}`);
+          const result = outputResults[outputFormat];
 
-    expect(genDiff(filePath1, filePath2)).toBe(stylishResult);
-  });
-
-  test('Should return correct diff for *.{yml,yaml} files', () => {
-    const filePath1 = getFixturePath('file1.yml');
-    const filePath2 = getFixturePath('file2.yml');
-
-    expect(genDiff(filePath1, filePath2)).toBe(stylishResult);
+          expect(genDiff(filePath1, filePath2, outputFormat)).toBe(result);
+        });
+      });
+    });
   });
 });
