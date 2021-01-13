b275c640d513313a3804ac0f7dc55b993d67fa6e
Kirill Kostikov <kirill.kostikov.dev@gmail.com>
2021-01-13 13:52:25 +0300

    add test for testing .{yml,yaml} files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -11,17 +11,23 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
-let jsonResult;
-
+let stylishResult;
 beforeAll(() => {
-  jsonResult = readFixture('result_json.txt');
+  stylishResult = readFixture('result_stylish.txt');
 });
 
-describe('gendiff', () => {
+describe('genDiff', () => {
   test('Should return correct diff for *.json files', () => {
     const filePath1 = getFixturePath('file1.json');
     const filePath2 = getFixturePath('file2.json');
 
-    expect(genDiff(filePath1, filePath2)).toBe(jsonResult);
+    expect(genDiff(filePath1, filePath2)).toBe(stylishResult);
+  });
+
+  test('Should return correct diff for *.{yml,yaml} files', () => {
+    const filePath1 = getFixturePath('file1.yml');
+    const filePath2 = getFixturePath('file2.yml');
+
+    expect(genDiff(filePath1, filePath2)).toBe(stylishResult);
   });
 });
